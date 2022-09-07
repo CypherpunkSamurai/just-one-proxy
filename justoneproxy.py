a0d631820ca80d9d9183ca8df32789fd88deba49
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2022-09-07 21:19:54 +0530

    Update justoneproxy.py
    Fix Content Size Bug

--- justoneproxy.py
+++ justoneproxy.py
@@ -5,14 +5,12 @@ from io import BytesIO, SEEK_END, SEEK_SET
 
 
 # Config
-proxy_file_url = "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt"
+proxy_file_url = "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt"
 proxy_regex = r'\d+\.\d+\.\d+\.\d+\:\d+'
 
 def _read_32bytes(stream_response) -> str:
     """
         Read the stream bytes content
-        
-        This reads 32 bytes from the response stream
     """
     # 
     content_bytes = BytesIO()
@@ -31,19 +29,25 @@ def _read_32bytes(stream_response) -> str:
     content_bytes.seek(_start)
     # return the bytes
     return content_bytes.read(_bytes_to_read).decode('utf-8')
+
+def _get_range(url):
+    """
+        Returns the Content Size in Bytes
+    """
+    r = requests.head(url, headers={"Accept-Encoding": "None"})
+    if not 'Content-Length' in r.headers:
+        #raise Exception("Content Length header not found...")
+        return 1024*10
+    return int(r.headers['Content-Length'])
     
 
 def get_proxy(url=proxy_file_url):
-    """
-      Syntax:
-          get_proxy(url="your_raw_git_file_url")
-        
-        Retuns:
-          "192.168.0.0:5000"
-    """
     # generate a random range
-    bytes_range = [ random.choice(range(1,9)) * (10**random.choice(range(0,5))) ]
+    bytes_range = [ random.choice(range(0, _get_range(url) - 32)) ]
     bytes_range.append(bytes_range[0] + 32)
+    #
+    print("File Range: %s" % _get_range(url))
+    print("Requested Content: %s" % bytes_range)
     # set range
     headers = {
         "Range": "bytes=%s-%s" % (bytes_range[0], bytes_range[1]),
@@ -57,5 +61,6 @@ def get_proxy(url=proxy_file_url):
     if content:
         proxy = re.findall(proxy_regex, content)
         if proxy:
+            # Return the proxy
             return proxy[0]
     return None
